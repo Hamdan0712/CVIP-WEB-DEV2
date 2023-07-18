906cd5911536a68f2ed37d96ae52856473cfc356
Hamdan0712 <107526113+Hamdan0712@users.noreply.github.com>
2023-07-18 08:30:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const addBtn=document.querySelector('.addTask-btn');
 const inputTask=document.querySelector('.taskinput');
 let pendingTask=document.querySelector('.taskleft-number');
+const taskleft=document.querySelector('.taskleft');
 const emptyText=document.querySelector('.empty-text');
 const taskContainer=document.querySelector('.taskAdd-container')
+
 const deleteTask=document.querySelector('.delete-btn');
 const mainContent=document.querySelector('.main-task');
 const checkbox=document.querySelector('#check-box');
 const listContainer=document.querySelector('#list-container');
+const app=document.querySelector('.app-main');
 
 
 
+ let taskcount=0;
+
+
+// let text=document.createElement("p");
+// text.innerText=`You Have ${listContainer.getElementsByTagName('li'.length)} task(s) to Complete `;
+
+// text.classList.add("taskleft");
+
+// app.appendChild(text)
 const addList=function(){
    const content=inputTask.value;
    if(!content){
@@ -27,9 +39,12 @@ const addList=function(){
       li.appendChild(span)
       
       emptyText.style.opacity='0';
+      const task=listContainer.getElementsByTagName("li").length;
+   console.log(task);
 
-
-
+      // text.innerText=`You Have ${task} task(s) to Complete`;
+      // pendingTask.innerHTML='';
+      pendingTask.innerHTML=` ${task}`;
        
 }
 inputTask.value='';
@@ -49,7 +64,13 @@ listContainer.addEventListener('click',function(e){
    }
    else if(e.target.tagName="SPAN"){
       e.target.parentElement.remove();
+      taskcount--;
+   
+   const task=listContainer.getElementsByTagName("li").length;
+   console.log(task);
 
+      // text.innerText=`You Have ${task} task(s) to Complete`;
+      pendingTask.innerHTML=` ${task}`;
 
    saveReload();
 }
@@ -62,12 +83,13 @@ listContainer.addEventListener('click',function(e){
 
 function saveReload(){
  localStorage.setItem("addListt ",listContainer.innerHTML);
-//  localStorage.setItem("taskcount",taskContainer.innerHTML);
+//  localStorage.setItem("taskh",text.innerText);
+ localStorage.setItem("taskh2",pendingTask.innerText)
 }
 
 function displayList(){
-//   listContainer.innerHTML=localStorage.getItem("data");
-  
+//   text.innerText=localStorage.getItem("taskh");
+  pendingTask.innerText=localStorage.getItem("taskh2")
   listContainer.innerHTML=localStorage.getItem("addListt ");
 }
 
